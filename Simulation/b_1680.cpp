14225e16cb5599dcd2296d5fe2f2d82778bd583e
kuony <wjdgycks20@naver.com>
2021-09-27 19:30:38 +0900

    롤러코스터

---
+++ Simulation/b_1680.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int testcase;
+	cin >> testcase;
+	for (int tc = 0; tc < testcase; tc++) {
+		int n, w;
+		cin >> w >> n;
+		for (int i = 0; i < n; i++) {
+
+		}
+	}
+}
